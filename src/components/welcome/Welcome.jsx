45028a6e26f12bb6a7d4353af3fe0e99e1f9b3ec
Andii Karamushko <karamushko1997@gmail.com>
2024-10-07 00:27:28 +0300

    changed video at welcomePage

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -19,7 +19,7 @@ const Welcome = ({onToggleModal}) => {
 
     const video = (
         <iframe
-            src="https://youtu.be/SwmRyntv9sM?si=Wh9AiE-9XOjpYeoz"
+            src="https://www.youtube.com/embed/SwmRyntv9sM"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
